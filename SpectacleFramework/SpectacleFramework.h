ebd0b4f50a4b3633dd7d72ff3009d27ad5ef0a2d
Sean Gilbertson <sgilbertson@ackmanndickenson.com>
2013-10-27 21:24:00 -0500

    Create SpectacleFramework target.

---
+++ SpectacleFramework/SpectacleFramework.h
@@ -0,0 +1,13 @@
+//
+//  SpectacleFramework.h
+//  SpectacleFramework
+//
+//  Created by Sean Gilbertson on 10/27/13.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SpectacleFramework : NSObject
+
+@end
---
+++ SpectacleFramework/SpectacleFramework.m
@@ -0,0 +1,13 @@
+//
+//  SpectacleFramework.m
+//  SpectacleFramework
+//
+//  Created by Sean Gilbertson on 10/27/13.
+//
+//
+
+#import "SpectacleFramework.h"
+
+@implementation SpectacleFramework
+
+@end
